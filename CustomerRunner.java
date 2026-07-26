3d449dc0da9aa46f21c523237f40116245ee73e7
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:48:41 +0530

    Customer main class

---
+++ CustomerRunner.java
@@ -0,0 +1,9 @@
+class CustomerRunner {
+
+    public static void main(String[] args) {
+        
+		System.out.println("Main Started");
+        Customer.address();
+        System.out.println("Main Ended");
+    }
+}
