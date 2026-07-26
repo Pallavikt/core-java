5d1bc8935a935f4c6762d7d8f445b1f3ef8f6a7b
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:49:08 +0530

    DeliveryAgent logic class

---
+++ DeliveryAgent.java
@@ -0,0 +1,11 @@
+class DeliveryAgent{
+	
+	public static void reachDoorStep(){
+		
+		System.out.println("reachDoorStep Started");
+		SignaturePortal.collectESign();
+		System.out.println("reachDoorStep Ended");
+	
+	}
+
+}
