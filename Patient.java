50bcce12113fd1e503f16679d891f8a17bc7dc61
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:56:18 +0530

    Patient logic class

---
+++ Patient.java
@@ -0,0 +1,9 @@
+class Patient{
+	
+	public static void sick(){
+		
+		System.out.println("sick satrted");
+		MobileApp.register("pallavi","hairfall","pallavi@gmail.com",82108723L);
+		System.out.println("sick ended");
+	}
+}
