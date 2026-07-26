8a2da2c4ab349a129f1c78fae35d7d65ca823fc8
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:55:54 +0530

    PackagingUnit logic class

---
+++ PackagingUnit.java
@@ -0,0 +1,10 @@
+class PackagingUnit{
+	
+	public static void packItem(){
+		
+		System.out.println("packItem Started");
+		LabelGenerator.printLabel();
+		System.out.println("packItem Ended");
+	}
+	
+}
