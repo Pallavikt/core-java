9fe2409a057c1d0cb72acfc66ecc132e6f15161a
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:57:46 +0530

    State logic class

---
+++ State.java
@@ -0,0 +1,10 @@
+class State{
+	
+	public static void getStateInfo(){
+		
+		System.out.println("getStateInfo Started");
+		City.getCityInfo();
+		System.out.println("getStateInfo Ended");
+	}
+	
+}
