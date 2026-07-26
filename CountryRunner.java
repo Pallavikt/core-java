8376198f28d174011ca4dcd20cb30fa9cec36469
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:47:16 +0530

    Country main class

---
+++ CountryRunner.java
@@ -0,0 +1,9 @@
+class CountryRunner {
+
+    public static void main(String[] args) {
+        
+		System.out.println("Main Started");
+        Countryy.run();
+        System.out.println("Main Ended");
+    }
+}
