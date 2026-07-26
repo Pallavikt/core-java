4f405bbc4aa7b395e4e7141514c740a2cc00bb26
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:53:33 +0530

    HubRouter logic class

---
+++ HubRouter.java
@@ -0,0 +1,11 @@
+class HubRouter{
+	
+	public static void sortToZipCode(){
+		
+		System.out.println("sortToZipCode Started");
+		DispatchVehicle.outOfDelivery();
+		System.out.println("sortToZipCode Ended");
+		
+	}
+	
+}
