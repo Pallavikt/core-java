3b192c258a40c582b3772dff3202d30a50360001
Pallavi K T <315pallavi@gmail.com>
2026-07-26 19:59:27 +0530

    User logic class

---
+++ User.java
@@ -0,0 +1,9 @@
+class User{
+	
+	public static void getUserInfo(){
+		
+		System.out.println("getUserinfo Started");
+		Address.getAddressInfo();
+		System.out.println("getUserInfo Ended");
+    }
+}
