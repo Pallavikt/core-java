2d6eb401b81648298773649497651f86e358961a
Pallavi K T <315pallavi@gmail.com>
2026-08-27 10:54:43 +0530

    Delete Hospitall.java

--- Hospitall.java
+++
@@ -1,9 +0,0 @@
-class Hospitall{
-	
-	public static void bookAppointment(String patientName,String symptom){
-		
-		System.out.println("bookAppointment satrted");
-		Nurse.basicCheck(patientName,symptom,false,false);
-		System.out.println("bookAppointment ended");
-	}
-}
