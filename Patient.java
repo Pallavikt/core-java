4157fb4169d299c6ac76e58892c76096feea3cbd
Pallavi K T <315pallavi@gmail.com>
2026-08-27 10:53:38 +0530

    Delete Patient.java

--- Patient.java
+++
@@ -1,9 +0,0 @@
-class Patient{
-	
-	public static void sick(){
-		
-		System.out.println("sick satrted");
-		MobileApp.register("pallavi","hairfall","pallavi@gmail.com",82108723L);
-		System.out.println("sick ended");
-	}
-}
