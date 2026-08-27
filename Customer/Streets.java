ace9b0fd6c087117b383c7080aa30de348ee70be
Pallavi K T <315pallavi@gmail.com>
2026-08-27 11:28:27 +0530

    Customer files

---
+++ Customer/Streets.java
@@ -0,0 +1,18 @@
+class Streets {
+
+    public static void details() {
+
+        System.out.println("details Started");
+
+        pincode();
+
+        System.out.println("details Ended");
+    }
+
+    public static void pincode() {
+
+        System.out.println("pincode Started");
+
+        System.out.println("pincode Ended");
+    }
+}
